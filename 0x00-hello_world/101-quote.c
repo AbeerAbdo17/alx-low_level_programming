08aec88ce69e2ed802ed0fe077ab90c3ad299056
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-07 04:02:45 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - enter point
+ *
+ *Description: A c program that print a line to standar a error
+ *
+ *Return: return 1 is (succes)
+*/
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
