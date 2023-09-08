174398d744d20e8d585076c9dcbeb20ee29bf458
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-08 06:41:56 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,18 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for (m = 0; m <= 9; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (l = 0; l <= 9; l++)
 			{
-				if (l > m && m < n)
+				if (n != m && n < m && m != l && m < l)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
+					putchar(n + 48);
+					putchar(m + 48);
+					putchar(l + 48);
+					if (n + m + l != 24)
 					{
 						putchar(',');
 						putchar(' ');
