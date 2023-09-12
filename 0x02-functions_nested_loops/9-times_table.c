92b6212b45bdafc439a227bc937a0afeb4142220
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-12 05:04:57 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,10 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(48);
-		for (j = 0; j < 10; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
