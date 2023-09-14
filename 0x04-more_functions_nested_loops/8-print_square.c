2f991e8965e8ef545717b263d3115e362b41d077
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-14 02:59:44 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,15 @@ void print_square(int size)
 {
 	int row, colum;
 
-	for (row = 1; row <= size; row++)
-	{
-		for (colum = 1; colum <= size; colum++)
-			_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (colum = 1; colum <= size; colum++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
