cf4971c9ff9dced06a1fe82ab941fa6975d5a493
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-18 02:13:28 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * rev_string - everses a string.
+ *
+ * @s: input parameter
+ *
+ * Return: nothing
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+	char tmp;
+
+	while (s[i] != '\0')
+		i++
+	for (j = 0; j < i; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = tmp;
+	}
+}
