9b0b1b9d1449b753e91d1fe253435e89957a022d
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-18 08:05:01 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rabd() % 78;
+		s = s + c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
