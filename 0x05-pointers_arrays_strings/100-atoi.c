8cdf2fd817503cde8b218e6fd7772c857ce3163b
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-18 07:32:30 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,13 @@ int _atoi(char *s)
 	int number = 0;
 	int si = 1;
 
-	while (*s++)
-	{
+	do {
 		if (*s == '-')
 			si = si * -1;
 		else if (*s >= '0' && *s <= '9')
 			number = (number * 10) + (*s - '0');
 		else if (number > 0)
 			break;
-	}
+	} while (*s++);
 	return (number * si);
 }
