5edf3a5519ec6ed0fa3c40bf9ebe123c28b3304f
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-09-20 08:19:06 -0700

    0x06. C - More pointers, arrays and strings C

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,15 @@
 
 void print_number(int n)
 {
-	int x = n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		x = -n;
 	}
-	if (x /10 != 0)
+	if (x / 10 != 0)
 		print_number(x / 10);
 
-	_putchar((x % 10) +48);
+	_putchar((x % 10) + 48);
 }
