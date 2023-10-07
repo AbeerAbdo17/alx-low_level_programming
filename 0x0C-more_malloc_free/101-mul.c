fb9f21126827f40ae8642ba0cd5a3dc190105250
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-07 08:57:09 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,19 +4,18 @@
 
 /**
  * check - digid or non digid
- *
- * @c: input
- *
+ * @c: input*
  * Return: 0 or 1
 */
 int check(char *c)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; c[x]; x++)
+	while (c[x])
 	{
-		if (c[x] < 48 || c[x] > 57)
+		if (c[x] < '0' || c[x] > '9')
 			return (0);
+		x++;
 	}
 	return (1);
 }
@@ -27,13 +26,12 @@ int check(char *c)
 */
 int len(char *c)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; c[x] != '\0'; x++)
-		;
-	return(x);
+	while (c[x] != '\0')
+		x++;
+	return (x);
 }
-
 /**
  * err - handel error
 */
@@ -44,23 +42,18 @@ void err(void)
 }
 /**
  * main -  multiplies two positive numbers
- *
  * @argc: input
  * @argv: input
- *
  * Return: noyhing
 */
-
 int main(int argc, char **argv)
 {
 	char *st1, *st2;
 	int lens1, lens2, l, x, crr, d1, d2, *res, b = 0;
 
-	st1 = argv[1];
-	st2 = argv[2];
-	if (argc != 3 || !check(st1) || check(st2))
+	st1 = argv[1], st2 = argv[2];
+	if (argc != 3 || !check(st1) || !check(st2))
 		err();
-
 	lens1 = len(st1);
 	lens2 = len(st2);
 	l = lens1 + lens2 + 1;
@@ -78,7 +71,7 @@ int main(int argc, char **argv)
 			d2 = st2[lens2] - '0';
 			crr = crr + res[lens1 + lens2 + 1] + (d1 * d2);
 			res[lens1 + lens2 + 1] = crr % 10;
-			crr = crr /10;
+			crr = crr / 10;
 		}
 		if (crr > 0)
 			res[lens1 + lens2 + 1] = res[lens1 + lens2 + 1] + crr;
