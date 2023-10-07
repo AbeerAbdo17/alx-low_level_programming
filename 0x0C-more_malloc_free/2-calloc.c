8f14e9b18084feddc12535d1a9238d908f32c2af
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-07 01:51:17 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,23 +1,5 @@
 #include"main.h"
 #include <stdlib.h>
-/**
- * *mem - file memory
- *
- * @p: pointer
- * @c: input
- * @m: number of copy
- *
- * Return: pointer
-*/
-
-char *mem(char *p, char c, unsigned int m)
-{
-	unsigned int x;
-
-	for (x = 0; x < m; x++)
-		p[x] = c;
-	return (p);
-}
 
 /**
  * *_calloc - allocates memory for an array, using malloc
