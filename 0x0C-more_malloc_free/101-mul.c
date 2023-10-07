bf69966d0dab71165d014390daea6cfa8fa8b446
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-07 08:22:27 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,84 +1,98 @@
-#include"main.h"
-#include<stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print - print string
+ * check - digid or non digid
  *
- * @s: pointer
+ * @c: input
  *
- * Return: nothing
+ * Return: 0 or 1
 */
-void print(char *s)
+int check(char *c)
 {
 	int x;
 
-	for (x = 0; s[x]; x++)
-		_putchar(s[x]);
+	for (x = 0; c[x]; x++)
+	{
+		if (c[x] < 48 || c[x] > 57)
+			return (0);
+	}
+	return (1);
 }
 /**
- * convert - convert string to intger
- * @x: input
- *
- * Return: convert
+ * len - return length
+ * @c: input
+ * Return: length
 */
-int convert(char *x)
+int len(char *c)
 {
-	int c = 1;
-	unsigned long int r = 0, f, y;
+	int x;
 
-	for (f = 0; !(x[f] >= 48 && x[f] <= 57); f++)
-	{
-		if (x[f] == '-')
-			c = c * -1;
-	}
-	for (y = f; x[y] >= 48 && x[y] <= 57; y++)
-	{
-		r = r * 10;
-		r = r + (x[y] - 48);
-	}
-	return (c * r);
-}
-
-/**
- * printint - print int
- *
- * @num: input
- *
- * Return: nothing
-*/
-void printint(unsigned long int num)
-{
-	unsigned long int d = 1, x, r;
-
-	for (x = 0; num / d > 9; x++, d *= 10)
+	for (x = 0; c[x] != '\0'; x++)
 		;
-	for (; d >= 1; num %= d, d /= 10)
-	{
-		r = num / d;
-		_putchar(48 + r);
-	}
+	return(x);
+}
+
+/**
+ * err - handel error
+*/
+void err(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 /**
- * main - print result
+ * main -  multiplies two positive numbers
  *
  * @argc: input
- * @argv: input list
+ * @argv: input
  *
- * Return: 0
+ * Return: noyhing
 */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	(void)argc;
+	char *st1, *st2;
+	int lens1, lens2, l, x, crr, d1, d2, *res, b = 0;
 
-	if (argc != 3)
+	st1 = argv[1];
+	st2 = argv[2];
+	if (argc != 3 || !check(st1) || check(st2))
+		err();
+
+	lens1 = len(st1);
+	lens2 = len(st2);
+	l = lens1 + lens2 + 1;
+	res = malloc(sizeof(int) * l);
+	if (res == NULL)
+		return (1);
+	for (x = 0; x <= lens1 + lens2; x++)
+		res[x] = 0;
+	for (lens1 = lens1 - 1; lens1 >= 0; lens1--)
 	{
-		print("Error\n ");
-		exit(98);
+		d1 = st1[lens1] - '0';
+		crr = 0;
+		for (lens2 = len(st2) - 1; lens2 >= 0; lens2--)
+		{
+			d2 = st2[lens2] - '0';
+			crr = crr + res[lens1 + lens2 + 1] + (d1 * d2);
+			res[lens1 + lens2 + 1] = crr % 10;
+			crr = crr /10;
+		}
+		if (crr > 0)
+			res[lens1 + lens2 + 1] = res[lens1 + lens2 + 1] + crr;
 	}
-
-	printint(convert(argv[1]) * convert(argv[2]));
+	for (x = 0; x < l - 1; x++)
+	{
+		if (res[x])
+			b = 1;
+		if (b)
+			_putchar(res[x] + '0');
+	}
+	if (!b)
+		_putchar('0');
 	_putchar('\n');
-
+	free(res);
 	return (0);
 }
