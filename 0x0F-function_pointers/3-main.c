a3bbda9a036d900e65ab9a6b20999bc03f7d88ca
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-11 05:38:46 -0700

    function_pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include"3-calc.h"
+
+/**
+ * main - main function
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int x, y;
+	char *p;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = atoi(argv[1]);
+	p = argv[2];
+	y = atoi(argv[3]);
+
+	if (!get_op_func(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*p == '/' && y == 0) || (*p == '%' && y == 0))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", get_op_func(p)(x, y));
+	return (0);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include"3-calc.h"
+/**
+ * op_add - add
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: difference
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: mul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: div
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
