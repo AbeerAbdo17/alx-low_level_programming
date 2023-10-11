4869367bee32be6d2a9556d376e259cde9129642
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-11 05:42:27 -0700

    function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	p = argv[2];
 	y = atoi(argv[3]);
 
-	if (!get_op_func(argv[2]))
+	if (!get_op_func(p) == NULL || p[1] != '\0')
 	{
 		printf("Error\n");
 		exit(98);
