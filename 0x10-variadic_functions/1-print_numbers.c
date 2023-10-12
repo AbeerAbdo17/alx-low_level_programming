e4a7900686ade2ad8cc208e0487791f283c311db
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-12 02:49:10 -0700

    variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int x = 0;
 	va_list p;
 
+	if (n == 0)
+	{
+		printf("\n");
+	}
 	va_start(p, n);
 	while (x < n)
 	{
