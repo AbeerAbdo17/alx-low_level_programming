52544e90d1dde6f7394b09bd49d6ba082a1d3f60
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-19 06:14:19 -0700

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-hello db "Hello, Holberton",10  ; The string to be printed, followed by a newline character
+msg db 'Hello, Holberton', 0xa, 0
 
 section .text
 global main
@@ -7,12 +7,6 @@ global main
 extern printf
 
 main:
-; Prepare the arguments for printf
-mov rdi, hello  ; The format string
-xor rax, rax   ; Clear RAX (no floating-point parameters)
-call printf    ; Call printf
-
-; Exit the program
-mov rax, 60     ; syscall number for exit (60)
-xor rdi, rdi    ; Status code, 0 in this case
-syscall
+mov rdi, msg
+mov eax, 0
+call printf
