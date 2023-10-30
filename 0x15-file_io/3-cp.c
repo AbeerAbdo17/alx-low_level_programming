3b1b27e651cb5fc0af6ada38a97a50f16ad92f3a
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-30 05:19:20 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,23 +13,23 @@ int main(int argc, char *argv[])
 	char buffer[BUFF_SIZE];
 
 	if (argc != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+		dprintf("Usage: %s file_from file_to\n", argv[0]), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf("Error: Can't read from file %s\n", argv[1]), exit(98);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf("Error: Can't write to file %s\n", argv[2]), exit(99);
 	while ((bytes = read(from, buffer, BUFF_SIZE)) > 0)
 	{
 		if (write(to, buffer, bytes) != bytes)
-			dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
+			dprintf("Error: Can't write to file %s\n", argv[2]), exit(99);
 	}
 	if (bytes == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf("Error: Can't read from file %s\n", argv[1]), exit(98);
 	if (close(from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", from), exit(100);
+		dprintf("Error: Can't close fd %d\n", from), exit(100);
 	if (close(to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", to), exit(100);
+		dprintf("Error: Can't close fd %d\n", to), exit(100);
 	return (0);
 }
