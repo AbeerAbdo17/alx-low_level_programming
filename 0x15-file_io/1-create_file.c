0640907329ac71c8f0ac529d1ab54a3410bf67c7
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-30 03:23:12 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t leng;
-	ssize_t byteswritten;
-
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
-	leng = 0;
+	ssize_t leng = 0;
+	ssize_t byteswritten = 0;
 
 	while (text_content[leng] != '\0')
 		leng++;
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 	if (leng)
 		byteswritten = write(fd, text_content, leng);
 	close(fd);
