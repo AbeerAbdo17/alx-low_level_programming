826bd8337ec3ff6822f7a5035df8dce5ee33f7c7
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-30 03:02:42 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (leng = 0; text_content[leng] != '\0'; leng++)
-			;
+		leng = 0;
+
+		while (text_content[leng] != '\0')
+			leng++;
 		byteswritten = write(fd, text_content, leng);
 		if (byteswritten == -1)
 		{
@@ -28,6 +32,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
+
 	close(fd);
 	return (-1);
 }
