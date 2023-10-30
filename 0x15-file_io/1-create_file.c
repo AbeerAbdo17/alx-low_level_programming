47edb5c5df87cb99ad70cc5a91bf2c65562645fe
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-30 03:26:58 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,7 @@ int create_file(const char *filename, char *text_content)
 		leng++;
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (leng)
