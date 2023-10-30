88b6d6183248b68f89e03630e3fad742a9a8a930
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-30 08:20:23 -0700

    wwwway

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,11 +21,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERM);
 	if (to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((bytes = read(from, buffer, BUFF_SIZE)) > 0)
 	{
 		if (write(to, buffer, bytes) != bytes)
-			dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (bytes == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
@@ -34,6 +34,6 @@ int main(int argc, char *argv[])
 	if (from)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 	if (to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 	return (EXIT_SUCCESS);
 }
