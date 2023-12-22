058ee3c5608c0f3b37118a38b1d473c8048667e4
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-22 07:03:26 -0800

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int x;
+	hash_node_t *crr, *xtp;
+
+	if (ht == NULL)
+		return;
+
+	for (x = 0; x < ht->size; x++)
+	{
+		crr = ht->array[x];
+		while (crr)
+		{
+			ctp = crr->next;
+			free(crr->key);
+			free(crr->value);
+			free(crr);
+			crr = xtp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
